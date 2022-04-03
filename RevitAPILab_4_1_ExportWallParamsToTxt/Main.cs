d0e621aa55684f3848d12d7c90c5e1ed40a30e9b
HinaChleck <HinaChleck@gmail.com>
2022-04-03 17:22:19 +0500

    Добавьте файлы проекта.

---
+++ RevitAPILab_4_1_ExportWallParamsToTxt/Main.cs
@@ -0,0 +1,66 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RevitAPILab_4_1_ExportWallParamsToTxt
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        //Экспортирует Марку и объемы стен в txt
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            #region Диалог сохранения файла
+            var saveFileDialog = new SaveFileDialog
+            {
+               OverwritePrompt = true,
+               InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+               Filter = "All files (*.*)|*.*",
+               FileName =   "wallInfo.txt",
+               DefaultExt = ".txt"
+            };
+
+            string selectedFilePath = string.Empty;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFilePath = saveFileDialog.FileName;
+            }
+
+            if (string.IsNullOrEmpty(selectedFilePath))
+                return Result.Cancelled;
+
+            #endregion
+
+            List<Wall> allWalls = new FilteredElementCollector(doc)
+                 .OfClass(typeof(Wall))
+                 .Cast<Wall>()
+                 .ToList();
+                                                  
+            string allText = string.Empty;
+            foreach (var wall in allWalls)
+            {
+           
+                string wallType = wall.LookupParameter("Марка").AsString();
+                string WallVolume = UnitUtils.ConvertFromInternalUnits(wall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble(), DisplayUnitType.DUT_CUBIC_METERS).ToString();
+                allText += $"{wallType}\t{WallVolume}{Environment.NewLine}";
+                              
+            }
+
+            File.WriteAllText(selectedFilePath, allText);
+            TaskDialog.Show("Экспорт", $"Файл сохранен по указанному пути:\n{selectedFilePath}\n{allText}");
+
+            return Result.Succeeded;
+        }
+
+    }
+}
---
+++ RevitAPILab_4_1_ExportWallParamsToTxt/WallsData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILab_4_1_ExportWallParamsToTxt
+{
+    private class WallsData
+    {
+        public string WallType { get; set; }
+        public string WallVolume { get; set; }
+    }
+}
